8af539b60f7f69b0a5d84120043fe84a593d172c
Quonic <spyingwind@gmail.com>
2013-09-24 11:16:51 -0500

    Create Stop-MyServices.ps1

---
+++ Stop-MyServices.ps1
@@ -0,0 +1,60 @@
+<#
+.Synopsis
+   Stop My list of services from a csv file
+.DESCRIPTION
+   This will stop a list of services based on the inputed csv file.
+   Check end of script for more info of what is required.
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Stop-MyServices
+{
+    [CmdletBinding()]
+    Param
+    (
+        # File import csv
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        $File,
+
+        # Sleep Time, defaults to 10 seconds
+        [int]
+        [Parameter(Mandatory=$false,
+            Position=1)]
+        $Time=10
+    )
+
+    Begin
+    {
+        if(-not (Test-Path $File)){
+            Write-Error "File Not Found: $File"
+            exit
+        }
+        $List = Import-Csv $File
+
+
+    }
+    Process
+    {
+                
+        for($i=0;$i -lt $List.Length;$i++){
+            $k = $List[$i].Server
+            $List[$i].Services.Split(';') | ForEach-Object {
+                (Get-Service -ComputerName $k -Name $_).Stop()
+            }
+            Start-Sleep -Seconds $Time
+        }
+    }
+    End
+    {
+    }
+}
+
+<#
+Example CSV to import
+
+Server,Services,
+Server1,Service1;Service2,
+#>
