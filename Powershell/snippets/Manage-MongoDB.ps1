caa883edae44de2f8c1902501e492cae4f4a4b9a
Jim Caten <spyingwind@gmail.com>
2017-04-27 14:33:20 -0500

    Create Manage-MongoDB.ps1

---
+++ Powershell/snippets/Manage-MongoDB.ps1
@@ -0,0 +1,59 @@
+
+Param([switch]$Install, [switch]$Remove)
+
+$mongoDbPath = "$PSScriptRoot\MongoDB"
+$mongoDbConfigPath = "$mongoDbPath\mongod.cfg"
+$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-v3.4-latest.zip" 
+$zipFile = "$mongoDbPath\mongo.zip" 
+$unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-ssl-v3.4-latest"
+
+if($Install){
+    if ((Test-Path -path $mongoDbPath) -eq $false)
+    {
+        Write-Host "Setting up directories..."
+        $temp = mkdir $mongoDbPath 
+        $temp = mkdir "$mongoDbPath\log" 
+        $temp = mkdir "$mongoDbPath\data" 
+        $temp = mkdir "$mongoDbPath\data\db"
+
+        Write-Host "Setting up mongod.cfg..."
+        [System.IO.File]::AppendAllText("$mongoDbConfigPath", "dbpath=$mongoDbPath\data\db`r`n")
+        [System.IO.File]::AppendAllText("$mongoDbConfigPath", "logpath=$mongoDbPath\log\mongo.log`r`n")
+        [System.IO.File]::AppendAllText("$mongoDbConfigPath", "smallfiles=true`r`n")
+
+        Write-Host "Downloading MongoDB..."
+        $webClient = New-Object System.Net.WebClient 
+        $webClient.DownloadFile($url,$zipFile)
+
+        Write-Host "Unblock zip file..."
+        Get-ChildItem -Path $mongoDbPath -Recurse | Unblock-File
+
+        Write-Host "Unzipping Mongo files..."
+        $shellApp = New-Object -com shell.application 
+        $destination = $shellApp.namespace($mongoDbPath) 
+        $destination.Copyhere($shellApp.namespace($zipFile).items())
+        
+        Copy-Item "$unzippedFolderContent\*" $mongoDbPath -recurse
+
+        Write-Host "Cleaning up..."
+        Remove-Item $unzippedFolderContent -recurse -force 
+        Remove-Item $zipFile -recurse -force
+
+        Write-Host "Installing Mongod as a service..."
+        & $mongoDBPath\bin\mongod.exe --config $mongoDbConfigPath --install
+        
+        Write-Host "Starting Mongod..."
+        & net start mongodb
+        
+    }
+    else {
+        Write-Host "MongoDB already installed."
+        Write-Host "Starting Mongod..."
+        & net start mongodb
+    }
+}
+if($Remove){
+    & $mongoDBPath\bin\mongod.exe --config $mongoDbConfigPath --remove
+    Remove-Item -Path $mongoDbPath -Recurse -Force
+    Write-Host "MongoDB Removed."
+}
