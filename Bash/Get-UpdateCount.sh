33ec706bf8ae5674f9ea28fad4ee5508982c7168
Spyingwind <spyingwind@gmail.com>
2024-06-20 19:44:18 -0500

    Adds Get-UpdateCount.sh

---
+++ Bash/Get-UpdateCount.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Description: Get the number of updates available
+
+# Exit code 100 indicates that there are updates available
+# Exit code 0 indicates that there are no updates available
+# Exit code 127 indicates that the script failed to find a supported package manager
+
+count=0
+if [ "$(command -v apt)" ]; then
+    # Check for updates using apt
+    apt update
+    # Get the number of available updates
+    count=$(apt list --upgradable 2>/dev/null | grep -c -E '[0-9]\.[0-9]')
+elif [ "$(command -v dnf)" ]; then
+    # Check for updates using dnf and get the number of available updates
+    count=$(dnf check-update 2>/dev/null | grep -c -E '[0-9]\.[0-9]')
+else
+    echo "Unsupported package manager"
+    # Exit with the error code 127 to indicate that the script failed to find a supported package manager
+    exit 127
+fi
+
+echo "$count"
+if [[ "${count}" == "0" ]]; then
+    # No updates available
+    exit 0
+else
+    # Use exit code 100 to indicate that there are updates available
+    exit 100
+fi
